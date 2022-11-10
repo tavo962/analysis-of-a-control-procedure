9de889c215fc9991395fb2205f2a97a3ac4456c3
Gustavo Martinez <gustavo@codeman.company>
2022-11-10 10:35:18 -0600

    Progress Linux

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# analysis-of-a-control-procedure
+# Práctica de análisis de procedimientos de control
+
+1. Presentar que es un pipe.
+2. Mostar que es AWK y como puede ayudarnos.
+3. Crear un dataset de reporte de usuarios.
+4. Crear un nuevo dataset csv a partir de uno grande.
--- commands.sh
+++ commands.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
+# Pipe
+
+cat list.txt | sort
+
+cat list.txt | sort -r
+
+
+cat list.txt | uniq
+
+
 # AWK es un lenguaje de programación diseñado para procesar datos basados en texto, ya sean ficheros o flujos de datos.
 
 awk -F: '{print $0}' /etc/passwd
 awk -F: '{print $1}' /etc/passwd
 
+cat /etc/passwd | awk -F: '{print $1}'
+
 # Varios comandos  a la vez
 cat /etc/passwd & ls -la
 
 # Conectar comandos con un pipe
-ls -la
